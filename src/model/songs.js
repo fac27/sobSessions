53a6c177ada1ee16a1a2c0c024527d3143c2e426
eliazzo <bethazz@hotmail.co.uk>
2023-06-06 18:17:36 +0100

    selectSongById() created to grab the unique song id which is necessary for the users to post their comment to the correct song

--- src/model/songs.js
+++ src/model/songs.js
@@ -11,4 +11,17 @@ const select_all_songs = db.prepare(/*sql*/ `
 
 export function getAllSongs() {
   return select_all_songs.all();
-}
+};
+
+const select_song_by_id = db.prepare(/*sql*/ `\
+SELECT id, title, artist, url FROM songs WHERE id = ?
+`);
+
+export function selectSongById(id) {
+  return select_song_by_id.get(id)
+};
+
+
+
+
+
