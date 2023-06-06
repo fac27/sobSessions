2bc4bfaaa0080852f89adae17ebc8364472863ab
Taha <tahafhassan@gmail.com>
2023-06-06 17:36:45 +0100

    stars component renders rating

--- src/routes/songs.js
+++ src/routes/songs.js
@@ -1,60 +1,17 @@
 import { layout } from "../template.js";
 import header from "./components/header.js";
-import iframe from "./components/iframe.js";
+import songsHTML from "./components/songsHTML.js";
 import { getAllSongs } from "../model/songs.js";
-
-
-
-const commentsArr = [
-  {
-    id: 1,
-    git_user_id: 1,
-    song_id: 1,
-    rating: 5,
-    comment: "I cried to this so much!!",
-  },
-  {
-    id: 2,
-    git_user_id: 1,
-    song_id: 1,
-    rating: 3,
-    comment: "Not that great to cry to :|",
-  },
-  {
-    id: 2,
-    git_user_id: 1,
-    song_id: 1,
-    rating: 2,
-    comment: "I barely cried at all",
-  },
-];
 
 export function get(req, res) {
   const title = `Top 10`;
   const songsArr = getAllSongs();
-  const songs = songsArr.map((song) => {
-    //const commentsArr = getSongComments(song.id);
-    return /*html*/ `
-        <div class="song-container flex">
-        <div class="player-container">
-        <p class="song--info">Song #${song.id}. ${song.artist}- ${song.title}</p>
-        ${iframe(song.url)}
-        </div>
-        <div class="comment-container">
-        ${commentsArr.map((comment) => {
-          return /*html*/ `
-            <div>
-            <p>${comment.rating}</p>
-            <p>${comment.comment}</p>
-            </div>`;
-        })}
-        </div>
-        </div>
-        `;
-  });
+  const songs = songsHTML(songsArr);
   const content = /*html*/ `
     ${header()}
+    <main class="all-songs-container">
     ${songs}
+    </main>
     `;
 
   const response = layout({ title, content });
