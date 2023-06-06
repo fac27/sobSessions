f508aa19221607f5a1b7d5c71a0a169ebf18c3a0
FomasTreeman <tom@team-freeman.com>
2023-06-06 15:53:26 +0100

    unsaved

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import * as token from './token.js';
@@ -6,14 +5,6 @@ import auth from './routes/auth.js';
 import * as home from './routes/home.js';
 import * as songs from './routes/songs.js';
 import dotenv from 'dotenv';
-=======
-import express from "express";
-import cookieParser from "cookie-parser";
-//import serverMiddleware from "./session.js";
-import * as home from "./routes/home.js";
-import * as songs from "./routes/songs.js";
-import dotenv from "dotenv";
->>>>>>> main
 dotenv.config();
 
 const server = express();
@@ -27,13 +18,8 @@ server.use(cookies);
 
 server.use(token.middleware);
 
-<<<<<<< HEAD
 server.get('/authentication', auth);
 server.get('/', home.get);
 server.get('/songs', songs.get);
-=======
-server.get("/", home.get);
-server.get("/songs", songs.get);
->>>>>>> main
 
 export default server;
