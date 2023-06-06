5fe60004726b57b229c84c8d65ce38d3cba6164d
Taha <tahafhassan@gmail.com>
2023-06-06 11:49:44 +0100

    styled main content box

--- src/routes/home.js
+++ src/routes/home.js
@@ -7,13 +7,15 @@ export function getHome(req, res){
     <div class="logo logo--login">
     <p class="logo-text--login">ಥ_ಥ</p>
     </div>
+    <div class="home-content-container">
     <h1>Top 10 songs to cry to...</h1>
-    <div>
+    <div class="places">
     <ul>
         <li>...in the bath</li>
         <li>...at the park</li>
         <li>...at the movies</li>
     </ul>
+    </div>
     </div>
     <button>Login w/ Github</button>
     </div>`
