f54fbb170241d1184198fad4dc4b96e0b3763e9e
FomasTreeman <tom@team-freeman.com>
2023-06-06 15:24:26 +0100

    res error

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,27 +2,28 @@ const client_id = process.env.CLIENT_ID;
 
 const LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=${client_id}`;
 
-const title = `ಥ_ಥ`;
-const content = /*html*/ `
-    <div class="logo logo--login">
-    <p>ಥ_ಥ</p>
-    </div>
-    <h1>Top 10 songs to cry to...</h1>
-    <div>
-    <ul>
-        <li>...in the bath</li>
-        <li>...at the park</li>
-        <li>...at the movies</li>
-    </ul>
-    </div>
-    <button>Login w/ Github</button>`;
+// const title = `ಥ_ಥ`;
+// const content = /*html*/ `
+//     <div class="logo logo--login">
+//     <p>ಥ_ಥ</p>
+//     </div>
+//     <h1>Top 10 songs to cry to...</h1>
+//     <div>
+//     <ul>
+//         <li>...in the bath</li>
+//         <li>...at the park</li>
+//         <li>...at the movies</li>
+//     </ul>
+//     </div>
+//     <button>Login w/ Github</button>`;
 
-const response = layout({ title, content });
+// const response = layout({ title, content });
 
-export function get() {
+export function get(req, res) {
   const user = req.signedCookies.user;
+  console.log(user);
   if (user) {
-    res.send(response);
+    res.send(`<h1>Welcome ${user.login}</h1>`);
   } else {
     res.send(`
       <h1>Welcome</h1>
