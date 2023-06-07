dd22477fbaa42c6207ea24d7077ab4e91ae262db
eliazzo <bethazz@hotmail.co.uk>
2023-06-07 15:23:55 +0100

    range start incomplete

--- src/routes/components/interactions-form.js
+++ src/routes/components/interactions-form.js
@@ -7,7 +7,16 @@ export default function interactionsForm(id) {
       <input name="comment"/>
 
       <label for="rating"></label>
-      <input name="rating" type="range" min="0" max="5"/>
+      <input 
+        style=""
+        class="rating" 
+        value="1" 
+        name="rating" 
+        type="range" 
+        min="0" 
+        max="5" 
+        oninput="this.style.setProperty('--value', this.value)"
+        />
     </div>
       <button type="submit">Submit</button>
     </form>
