edaca064379f006986a631bea5b13c7250890434
FomasTreeman <tom@team-freeman.com>
2023-06-07 08:57:56 +0100

    token validation

--- src/token.js
+++ src/token.js
@@ -1,4 +1,6 @@
-export function middleware(req, res, next) {
+const VALIDATE_URL = 'https://api.github.com/applications/validate_token';
+
+export async function middleware(req, res, next) {
   if (
     !req.signedCookies?.name ||
     !req.signedCookies?.refresh_token ||
@@ -6,23 +8,34 @@ export function middleware(req, res, next) {
   )
     return next();
 
-  // either store acces_tokens in db with an expiry or check valid access token with fetch
+  // either store access_tokens in db with an expiry or check valid access token with fetch
+  async function validToken() {
+    const body = req.signedCookies;
+    return await fetch(VALIDATE_URL, {
+      method: 'post',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((json) => json.valid);
+  }
 
-  // const user = req.signedCookies.user;
-  console.log(req.signedCookies);
-  // if (user) {
-  //   logout(req, res);
-  //   return next();
-  // } else {
-  req.session = req.signedCookies;
-  // }
+  if (!(await validToken())) {
+    logout(req, res);
+    return next();
+  } else {
+    req.session = req.signedCookies;
+  }
   return next();
 }
 
 function logout(req, res) {
-  console.log("logging out");
-  res.clearCookie("refresh_token");
-  res.clearCookie("access_token");
-  res.clearCookie("user");
-  res.redirect("/");
+  console.log('logging out');
+  res.clearCookie('refresh_token');
+  res.clearCookie('access_token');
+  res.clearCookie('user');
+  res.redirect('/');
 }
