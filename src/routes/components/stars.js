4511805a4590a6a1c930d9ace88cdb673c2ab99a
hanleymark <home@markhanley.co.uk>
2023-06-07 16:29:34 +0100

    Add aria labels to no comment emoji and star rating

--- src/routes/components/stars.js
+++ src/routes/components/stars.js
@@ -4,10 +4,12 @@ export default function stars(number) {
   let rating = ``;
 
   for (let i = 0; i < goldStars; i++) {
-    rating += `<i class="fa-solid fa-star" style="color: #d7a63c;"></i>`;
+    rating += `<i class="fa-solid fa-star" style="color: #d7a63c;" aria-hidden="true"></i>`;
   }
   for (let i = 0; i < greyStars; i++) {
-    rating += `<i class="fa-solid fa-star" style="color: #636363;"></i>`;
+    rating += `<i class="fa-solid fa-star" style="color: #636363;" aria-hidden="true"></i>`;
   }
-  return rating;
+  return `<figure aria-label="song rated ${number} star${
+    number > 1 ? 's' : ''
+  }">${rating}</figure>`;
 }
