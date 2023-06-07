606599847271bfaa7c4952ca5262663af81f691c
FomasTreeman <tom@team-freeman.com>
2023-06-07 11:40:21 +0100

    interactions removed timestamp

--- src/model/interactions.js
+++ src/model/interactions.js
@@ -1,16 +1,8 @@
-import db from "../database/db.js";
-
-const get_avg_ratings = db.prepare(/*sql*/ `
-SELECT AVG(rating) AS avg
-FROM interactions`);
-
-export function getAverageRatings() {
-  return get_avg_ratings.get();
-}
+import db from '../database/db.js';
 
 const insert_interaction = db.prepare(/*sql*/ `
-  INSERT into interactions (song_id, rating, comment, created_at)
-  VALUES ($song_id, $rating, $comment, $created_at)
+  INSERT into interactions (song_id, rating, comment)
+  VALUES ($song_id, $rating, $comment)
 `);
 
 export function createInteraction(interaction) {
