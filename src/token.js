6588d6c107022afeebf102343769f4dd21498f59
FomasTreeman <tom@team-freeman.com>
2023-06-08 08:27:09 +0100

    checking still works and get user

--- src/token.js
+++ src/token.js
@@ -11,6 +11,7 @@ dotenv.config({ path: process.cwd() + '/.env' });
 console.log(process.env.GITHUB_TOKEN);
 
 export async function middleware(req, res, next) {
+  req.sessionIsValid = true;
   function checkCookies(object) {
     let bool = true;
     if (!object?.name || !object?.refresh_token || !object?.access_token)
@@ -23,40 +24,44 @@ export async function middleware(req, res, next) {
   //   !req.signedCookies?.refresh_token ||
   //   !req.signedCookies?.access_token
   // )
-  if (!hasCookies) return next();
+  if (!hasCookies) {
+    req.sessionIsValid = false;
+    return next();
+  }
 
-  // either store access_tokens in db with an expiry or check valid access token with fetch
-  async function validateToken() {
-    const body = { access_token: req.signedCookies.access_token };
-    console.log({ access_token: req.signedCookies.access_token });
-    return await fetch(VALIDATE_URL, {
-      method: 'POST',
-      headers: {
-        accept: 'application/vnd.github+json',
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-      },
-      body: JSON.stringify(body),
-    })
-      .then((res) => res.status)
-      .catch((error) => console.log(error));
-  }
-  const validateTokenResponse = await validateToken();
-  console.log('validate message: ', validateTokenResponse);
-  if (validateTokenResponse == 404) {
-    console.log('invalid token');
-    // logout(req, res);
-    req.sessionIsValid = false;
-    return next();
-  } else if (validateTokenResponse == 200) {
-    console.log('valid token');
-    req.sessionIsValid = true;
-    return next();
-  } else {
-    console.log('error', validateTokenResponse);
-    console.log('req status: ', req.status);
-    req.sessionIsValid = false;
-    return next();
-  }
+  // // either store access_tokens in db with an expiry or check valid access token with fetch
+  // async function validateToken() {
+  //   const body = { access_token: req.signedCookies.access_token };
+  //   console.log({ access_token: req.signedCookies.access_token });
+  //   return await fetch(VALIDATE_URL, {
+  //     method: 'POST',
+  //     headers: {
+  //       accept: 'application/vnd.github+json',
+  //       Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+  //     },
+  //     body: JSON.stringify(body),
+  //   })
+  //     .then((res) => res.status)
+  //     .catch((error) => console.log(error));
+  // }
+  // const validateTokenResponse = await validateToken();
+  // console.log('validate message: ', validateTokenResponse);
+  // if (validateTokenResponse == 404) {
+  //   console.log('invalid token');
+  //   // logout(req, res);
+  //   req.sessionIsValid = false;
+  //   return next();
+  // } else if (validateTokenResponse == 200) {
+  //   console.log('valid token');
+  //   req.sessionIsValid = true;
+  //   return next();
+  // } else {
+  //   console.log('error', validateTokenResponse);
+  //   console.log('req status: ', req.status);
+  //   req.sessionIsValid = false;
+  //   return next();
+  // }
+  return next();
 }
 
 function logout(req, res) {
@@ -64,5 +69,6 @@ function logout(req, res) {
   res.clearCookie('refresh_token');
   res.clearCookie('access_token');
   res.clearCookie('name');
-  res.redirect('/');
+  req.sessionIsValid = false;
+  // res.redirect('/');
 }
